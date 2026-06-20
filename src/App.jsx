80e6ed9f4d39a47e20cbfd76bf387ea26aa22fa5
rahul <rrahul62808@gmail.com>
2026-06-20 13:08:55 +0530

    add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
 
-import './App.css'
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div>CPMS -Frontend</div>
+    <div className=''>CPMS -Frontend</div>
     </>
   )
 }
