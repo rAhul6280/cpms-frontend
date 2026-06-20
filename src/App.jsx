9d92ba880122540e42e64f7c30840520b080fdea
rahul <rrahul62808@gmail.com>
2026-06-20 12:44:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>CPMS -Frontend</div>
+    </>
+  )
+}
+
+export default App
